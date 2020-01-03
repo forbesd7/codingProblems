521f57269f7eb72ae83782ecaeaeda92dcc53ab7
Derek <derek.forbes77@gmail.com>
2020-01-03 16:02:44 +0900

    starting oneAway

---
+++ oneAway.js
@@ -0,0 +1,16 @@
+const str1 = "pale";
+const str2 = "ple";
+
+const str3 = "pales";
+const str4 = "pale";
+
+const str5 = "pale";
+const str6 = "bale";
+
+const str7 = "pale";
+const str8 = "bake";
+
+const oneAway = (str1, str2) => {
+  if (str1.length === str2.length) {
+  }
+};
